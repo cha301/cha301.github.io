cc109272ab4ffbf1d1939a197039a3aef4f5dbd6
cha301 <calumpangcharice@gmail.com>
2025-06-28 11:12:53 +0800

    initial commit

---
+++ assets/scripts/main.js
@@ -0,0 +1,16 @@
+const firstnum= 100
+const secnum= 3
+
+console.log("Hello World")
+
+
+function myMult(x,y){
+    return x/y
+
+}
+console.log(myMult(firstnum,secnum))
+
+for(let x = 0; x < 100; x = x + 2) {
+console.log(x)
+
+}
